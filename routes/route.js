5ce7cea5308fdf6a6f7033c597f1a16b94e88c8b
Kaushal <kaushal.dhruw@talentica.com>
2018-02-23 18:33:01 +0530

    Added few more details and swagger

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,8 @@
+exports.index = function(req, res) {
+    //res.send('<h1>Welcome to this page</h1>');
+    res.render('index', {});
+};
 
-exports.index = function(req,res){
-    res.send('<h1>Welcome to this page</h1>');
-}
-
-exports.index2 = function(req,res){
-    res.render('index',{});
-}
+// exports.index2 = function(req,res){
+//     res.render('index',{});
+// }
